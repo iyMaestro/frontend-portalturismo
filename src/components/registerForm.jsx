b473889058d433f9a7458a445460661b688df2ef
iyMaestro <flaxx995@gmail.com>
2025-08-01 14:49:43 -0300

    update

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -10,7 +10,7 @@ const RegisterForm = () => {
        
 
        try {
-        const response = await axios.post("http://localhost:5000/api/users",{
+        const response = await axios.post("https://backend-portalturismo-omjp.onrender.com/api/users",{
             name: nome,
             email,
             password :  senha
